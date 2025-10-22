b2006c8ebb117a90f4dec2083c17fb5004adffcb
rokstr86 <rokstr24@gmail.com>
2025-10-21 19:02:36 -0500

    build: use @tailwindcss/postcss plugin + pin Next stable

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,6 @@
-export default { plugins: { tailwindcss: {}, autoprefixer: {} } }
+﻿import tailwind from "@tailwindcss/postcss"
+import autoprefixer from "autoprefixer"
+
+export default {
+  plugins: [tailwind(), autoprefixer()],
+}
